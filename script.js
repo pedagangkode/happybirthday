1f448fdd2ff05b81fbdf64de3039b0aae57b9306
pedagangkode <piankewer@gmail.com>
2023-08-23 12:20:11 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,25 @@
+// alert
+
+function showCustomAlert() {
+  // custom pesan untuk pasangan mu
+  swal("SELAMAT ULANG TAHUN SAYANG ❤️");
+
+}
+
+
+ // JavaScript animasi pengetikan
+
+ document.addEventListener("DOMContentLoaded", function () {
+  new TypeIt(".typewriter", {
+    strings: [""],
+    speed:80
+  }).go();
+});
+
+
+// script lagu
+// var audio = new Audio('ultah.mp3');
+
+// window.addEventListener('load', function() {
+//   audio.play();
+// });
